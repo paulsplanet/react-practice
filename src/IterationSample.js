d2d1766031e0ba66d4b6adf3d424e3e2b877df30
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-07-18 15:24:36 -0700

    iterartion practice 2

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IterationSample = () => {
-    const names = ["blizzard", "ice", "thunder", "rain"];
+    const [names, setNames] = useState([
+        { id: 1, text: "blizzard" },
+        { id: 2, text: "ice" },
+        { id: 3, text: "rain" },
+        { id: 4, text: "wind" }
+    ]);
+    const [inputText, setInputText] = useState();
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = e => setInputText(e.target.value);
+
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId, text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText("")
+    };
+
+    const onRemove = (id) => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
+
     return (
-        <ul>
-            {names.map((name, index) => (<li key={index}>{name}</li>))}
-        </ul>
+        <>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>Add</button>
+            <ul>
+                {names.map((name) => (<li key={name.id} onDoubleClick={() => onRemove(name.id)} >{name.text}</li>))}
+            </ul>
+        </>
     )
-}
+};
 
 export default IterationSample;
