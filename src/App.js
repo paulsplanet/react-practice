0f2e46ca26f3313958cda3df28b35bf079010f8b
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-07-19 20:26:08 -0700

    lifecycle practice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 
 
 
 // <MyComponent name={"Luna"} favoriteNumber={7}>React</MyComponent> = comopenet practice
 
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: "#000000"
+  }
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  }
+
   render() {
       return (
-        <IterationSample />
+        <div>
+          <button onClick={this.handleClick}>Random Color</button>
+          <LifeCycleSample color={this.state.color} />
+        </div>
       )
   }
   
